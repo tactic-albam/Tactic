a21c733c01b70054b102e2f1a86e70f1e531942d
albam@tacticlogistics.com <albam@tacticlogistics.com>
2016-12-15 15:12:03 -0500

    Inicio proyecto tactic-connect

---
+++ tactic-integrador-db-stage/src/main/java/com/tacticlogistics/integrador/dbstage/TacticIntegradorDbStageApplication.java
@@ -0,0 +1,12 @@
+package com.tacticlogistics.integrador.dbstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacticIntegradorDbStageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacticIntegradorDbStageApplication.class, args);
+	}
+}
